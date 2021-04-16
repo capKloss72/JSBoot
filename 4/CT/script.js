723f5fe7c44c721bf5e951d1607c6d8487528d44
capkloss72 <tym.zon@gmail.com>
2021-04-16 16:32:10 +0800

    Started coding task, need to find out how to collect from submit

---
+++ 4/CT/script.js
@@ -0,0 +1,82 @@
+
+// Create list item
+function generateNoteListItem(noteName) {
+
+  var list = document.getElementById('list');
+  var newLi = document.createElement('li');
+  var newP = document.createElement('p');
+  var newPI = document.createElement('p');
+  var newIPencil = document.createElement('i');
+  var newITimes = document.createElement('i');
+  var newInput = document.createElement('input');
+
+  newLi.appendChild(newP);
+  newLi.appendChild(newPI);
+  newPI.appendChild(newIPencil);
+  newPI.appendChild(newITimes);
+  newLi.appendChild(newInput);
+
+  newP.innerHTML = noteName;
+  newIPencil.setAttribute('class', 'fa fa-pencil-square-o');
+  newITimes.setAttribute('class', 'fa fa-times');
+  newInput.setAttribute('class', 'edit-node');
+  newInput.setAttribute('type', 'text');
+
+  console.log(newLi);
+
+//   var newEl = document.createElement('button');
+// console.log(newEl);
+
+// var text = document.createTextNode('Click');
+
+// newEl.appendChild(text);
+// newEl.setAttribute('style', 'display: block; margin: 10px auto; padding: 5px 10px; background: coral; color: white');
+
+// var form = document.getElementById('add');
+// //form.appendChild(newEl);
+
+// form.insertBefore(newEl, form.children[0]);
+
+// form.removeChild(newEl);
+
+
+
+//<li>
+//					<p>Third note</p>
+//					<p><i class="fa fa-pencil-square-o"></i><i class="fa fa-times"></i></p>
+//					<input class="edit-note" type="text">
+//				</li>
+}
+
+function getUserInput() {
+  var btn = document.getElementById('add-btn');
+  var input = document.getElementById('add-input');
+  
+  btn.addEventListener('click', function() {
+    //if (input.textContent)
+    console.log(input.nodeValue);
+  })
+}
+
+var btn = document.getElementById('add-btn');
+var input = document.getElementById('add-input');
+  
+btn.addEventListener('click', function() {
+  //if (input.textContent)
+  
+  console.log('tuym');
+  generateNoteListItem('Tymek');
+})
+
+//generateNoteListItem('First Note');
+//getUserInput();
+
+
+
+
+
+
+
+
+
+
