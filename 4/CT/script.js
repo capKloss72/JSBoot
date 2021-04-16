5d743e94de9ae866bd388b44446d8722e33ae5fb
Tym Zon <tym.zon@gmail.com>
2021-04-16 17:47:42 +0800

    Added check for missing vale

--- 4/CT/script.js
+++ 4/CT/script.js
@@ -24,52 +24,20 @@ function generateNoteListItem(noteName) {
 
 	list.appendChild(newLi);
 
-	console.log(newLi);
-	
-//   var newEl = document.createElement('button');
-// console.log(newEl);
-
-// var text = document.createTextNode('Click');
-
-// newEl.appendChild(text);
-// newEl.setAttribute('style', 'display: block; margin: 10px auto; padding: 5px 10px; background: coral; color: white');
-
-// var form = document.getElementById('add');
-// //form.appendChild(newEl);
-
-// form.insertBefore(newEl, form.children[0]);
-
-// form.removeChild(newEl);
-
-
-
-//<li>
-//					<p>Third note</p>
-//					<p><i class="fa fa-pencil-square-o"></i><i class="fa fa-times"></i></p>
-//					<input class="edit-note" type="text">
-//				</li>
+	//console.log(newLi);
 }
 
-function getUserInput() {
-  var btn = document.getElementById('add-btn');
-  var input = document.getElementById('add-input');
-  
-  btn.addEventListener('click', function() {
-    console.log(input.nodeValue);
-  })
-}
 
 var btn = document.getElementById('add-btn');
 var input = document.getElementById('add-input');
   
 btn.addEventListener('click', function(e) {
 	e.preventDefault();
-  console.log('Nothing ' + input.value);
-  generateNoteListItem(input.value);
+	var newNote = input.value;
+	if (newNote != null && newNote != '') {
+		generateNoteListItem(input.value);
+	}
 })
-
-//generateNoteListItem('input.value');
-
 
 
 
