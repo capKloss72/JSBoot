2b5744e251b2c281e66b726f3e39804eb1ac173f
capkloss72 <tym.zon@gmail.com>
2021-04-16 15:02:28 +0800

    started events part 3 - event bubbling, multiple events fired on different tree elements

--- 4/script.js
+++ 4/script.js
@@ -1,4 +1,4 @@
-var h2 = document.querySelector('header h2');
+//var h2 = document.querySelector('header h2');
 
 // h2.onclick = function() {
 //   console.log('onclick executed');
@@ -8,13 +8,38 @@ var h2 = document.querySelector('header h2');
 //   console.log('mouseover executed');
 // }
 
-h2.addEventListener('click', function() {
-  console.log('Clicked');
-});
+// h2.addEventListener('click', function() {
+//   console.log('Clicked');
+// });
 
-h2.addEventListener('click', a);
+// h2.addEventListener('click', function () {
+//   a(5, 10);
+// });
 
-function a() {
-  console.log('A Clicked');
+// function a(x, y) {
+//   console.log(x + y);
+// }
+
+var div = document.querySelector('div.wrapper');
+var header = document.querySelector('header');
+var h2 = document.querySelector('header h2');
+
+function a (message) {
+  console.log(message);
 }
+
+div.addEventListener('click', function () {
+  a ('div clicked');
+})
+
+header.addEventListener('click', function () {
+  a ('header clicked');
+})
+
+h2.addEventListener('click', function () {
+  a ('h2 clicked');
+})
+
+
+
 
