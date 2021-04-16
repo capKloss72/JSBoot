7cd7912c153fb42f1207a1d179945fc5005c94fa
capkloss72 <tym.zon@gmail.com>
2021-04-16 14:39:59 +0800

    started events part 2 - add event listener

--- 4/script.js
+++ 4/script.js
@@ -1,5 +1,20 @@
 var h2 = document.querySelector('header h2');
 
-h2.onclick = function() {
-  console.log('onclick executed');
+// h2.onclick = function() {
+//   console.log('onclick executed');
+// }
+
+// h2.onmouseover = function() {
+//   console.log('mouseover executed');
+// }
+
+h2.addEventListener('click', function() {
+  console.log('Clicked');
+});
+
+h2.addEventListener('click', a);
+
+function a() {
+  console.log('A Clicked');
 }
+
