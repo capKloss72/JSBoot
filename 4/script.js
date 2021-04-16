d7abbbea374c39bd8f70e60172ca854e7bdaa760
capkloss72 <tym.zon@gmail.com>
2021-04-16 14:27:49 +0800

    started events part 1

--- 4/script.js
+++ 4/script.js
@@ -1,46 +1,5 @@
-//var icons = document.getElementsByClassName('fa');
-
-// for (var i = 0; i < icons.length; i++) {
-//   console.log(icons[i]);
-// }
-
-// var iconsarray = Array.from(icons);
-// iconsarray.push('hello');
-
-// console.log(iconsarray);
-
-// Array.from(icons).forEach(function(icon) {
-//   console.log(icon);
-// })
-
-// var lis = document.getElementsByTagName('li');
-// console.log(lis);
-
-// var lis = document.querySelectorAll('li, h2, #hide-list');
-// console.log(lis);
-
-// Array.from(lis).forEach(function(li) {
-//   li.textContent = 'Hello world';
-// });
-
 var h2 = document.querySelector('header h2');
 
-h2.className = 'changeBg';
-h2.className = 'changeFt';
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+h2.onclick = function() {
+  console.log('onclick executed');
+}
