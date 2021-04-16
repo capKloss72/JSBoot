6054784ebcaabfd41902e0a235bbd15f039fdb42
capkloss72 <tym.zon@gmail.com>
2021-04-16 15:27:55 +0800

    basic attribte manipulation

--- 4/script.js
+++ 4/script.js
@@ -1,45 +1,8 @@
-//var h2 = document.querySelector('header h2');
+var btn = document.getElementById('add-btn');
+var input = document.getElementById('add-input');
 
-// h2.onclick = function() {
-//   console.log('onclick executed');
-// }
-
-// h2.onmouseover = function() {
-//   console.log('mouseover executed');
-// }
-
-// h2.addEventListener('click', function() {
-//   console.log('Clicked');
-// });
-
-// h2.addEventListener('click', function () {
-//   a(5, 10);
-// });
-
-// function a(x, y) {
-//   console.log(x + y);
-// }
-
-var div = document.querySelector('div.wrapper');
-var header = document.querySelector('header');
-var h2 = document.querySelector('header h2');
-
-function a (message) {
-  console.log(message);
-}
-
-div.addEventListener('click', function () {
-  a ('div clicked');
-})
-
-header.addEventListener('click', function () {
-  a ('header clicked');
-})
-
-h2.addEventListener('click', function () {
-  a ('h2 clicked');
-})
-
-
-
-
+btn.addEventListener('click', function (e) {
+  e.preventDefault();
+  input.setAttribute('type', 'submit');
+  input.setAttribute('value', input.value);
+});
