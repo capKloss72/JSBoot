a77095206fe816ef54621b55ce042ba419687f10
capkloss72 <tym.zon@gmail.com>
2021-04-19 15:07:57 +0800

    QUIZ App: Started on dynamically adding input

--- QuizApp/script.js
+++ QuizApp/script.js
@@ -68,7 +68,6 @@ var quizContraller = (function() {
             getStoredQuestions = questionLocalStorage.getQuestionCollection();
             getStoredQuestions.push(newQuestion);
             questionLocalStorage.setQuestionCollection(getStoredQuestions);
-
             newQuestionText.value = "";
             options.forEach(option => {
               if (option.value !== null && option.value !== "") {
@@ -99,11 +98,20 @@ var UIController = (function() {
     // Admin Panel Elements
     questionInsertBtn: document.getElementById('question-insert-btn'),
     newQuestionText: document.getElementById('new-question-text'),
-    adminOption: document.querySelectorAll('.admin-option')
+    adminOption: document.querySelectorAll('.admin-option'),
+    adminOptionsContainer: document.querySelector('.admin-options-container')
   }
   
   return {
-    getDomItems: domItems
+    getDomItems: domItems,
+    addInoutsDynamically: function() {
+
+      addInput = function () {
+        console.log('added input dynamically');
+      }
+
+      domItems.adminOptionsContainer.lastElementChild.lastElementChild.addEventListener('focus', addInput);
+    }
   };
 
 })();
@@ -111,6 +119,9 @@ var UIController = (function() {
 //****CONTROLLER****/
 var Controller = (function(quizCtrl, uiCtrl) {
   var selectedDomItems = uiCtrl.getDomItems;
+
+  uiCtrl.addInoutsDynamically();
+
   selectedDomItems.questionInsertBtn.addEventListener('click', function(e) {
     quizCtrl.addQuestionOnLocalStorage(selectedDomItems.newQuestionText, selectedDomItems.adminOption);
   })  
