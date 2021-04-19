86aafe1f5ecfd5c2e32f7514567d84dee5a46f50
capkloss72 <tym.zon@gmail.com>
2021-04-19 14:07:34 +0800

    QUIZ App: Added methods to store objects in local storage

--- QuizApp/script.js
+++ QuizApp/script.js
@@ -11,23 +11,51 @@ var quizContraller = (function() {
     }
   }
 
-  return {
-    addQuestionOnLocalStorage: function (newQuestionText, options) {
-      var optionsArr, corrAnswer, questionId, newQuestion;
-      questionId = 0;
-      options.forEach(option => {
-        if (option.value !== null && option.value !== "") {
-          optionsArr.push(option.value);
-        }
-        if (option.previousElementSibling.checked && option.value !== "") {
-          corrAnswer = option.value;
-        }
-      });
-      var newQuestion = new Question(1,newQuestionText,optionsArr,corrAnswer);
-      localStorage.setItem(questionId, JSON.stringify(newQuestion));
+  var questionLocalStorage = {
+    setQuestionCollection: function(newCollection) {
+      localStorage.setItem('questionCollection', JSON.stringify(newCollection))     
+    },
+    getQuestionCollection: function() {
+      var quuestionCollection = JSON.parse(localStorage.getItem('questionCollection'))
+      return quuestionCollection;
+    },
+    removeQuestionCollection: function() {
+      localStorage.removeItem('questionCollection')
     }
   }
 
+  return {
+    addQuestionOnLocalStorage: function (newQuestionText, options) {
+      var optionsArray, correctAnswer, questionId, newQuestion, getStoredQuestions;
+
+      if (questionLocalStorage.getQuestionCollection() === null) {
+        questionLocalStorage.setQuestionCollection([]);
+      }
+
+      optionsArray = [];
+      options.forEach(option => {
+        if (option.value !== null && option.value !== "") {
+          optionsArray.push(option.value);
+        }
+        if (option.previousElementSibling.checked && option.value !== "") {
+          correctAnswer = option.value;
+        }
+      });
+
+      const qCollection = questionLocalStorage.getQuestionCollection();
+      // [] 
+      if(questionLocalStorage.getQuestionCollection().length > 0) {
+        questionId = questionLocalStorage.getQuestionCollection()[questionLocalStorage.getQuestionCollection().length - 1].id + 1;
+      } else {
+        questionId = 0;
+      }
+      var newQuestion = new Question(questionId, newQuestionText, optionsArray, correctAnswer);
+      getStoredQuestions = questionLocalStorage.getQuestionCollection();
+      getStoredQuestions.push(newQuestion);
+      questionLocalStorage.setQuestionCollection(getStoredQuestions);
+      console.log('qCollection.getQuestionCollection() :>> ', questionLocalStorage.getQuestionCollection());
+    }
+  }
 })();
 
 //****UI CONTROLLER****/
@@ -39,7 +67,7 @@ var UIController = (function() {
     newQuestionText: document.getElementById('new-question-text'),
     adminOption: document.querySelectorAll('.admin-option')
   }
-
+  
   return {
     getDomItems: domItems
   };
@@ -51,6 +79,5 @@ var Controller = (function(quizCtrl, uiCtrl) {
   var selectedDomItems = uiCtrl.getDomItems;
   selectedDomItems.questionInsertBtn.addEventListener('click', function(e) {
     quizCtrl.addQuestionOnLocalStorage(selectedDomItems.newQuestionText.value, selectedDomItems.adminOption);
-  })
-  
+  })  
 })(quizContraller, UIController);
